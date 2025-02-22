cb10f2508e8fbb2b79eca868432295de998b06fb
AnasSabbag <86312422+AnasSabbag@users.noreply.github.com>
2025-02-22 23:54:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+function handleSubmit() {
+	const fName=document.getElementById("first-name").value;
+	const lName=document.getElementById("last-name").value;
+	const phone=document.getElementById("phone").value;
+	const email=document.getElementById("email").value;
+
+	
+	const alrt=`First Name: ${fName} Last Name: ${lName} Phone Number: ${phone} Email ID: ${email}`;
+	alert(alrt);
+	
+}
