1645e71e033c3bb0cc0018381cf50e8602506f98
AnasSabbag <86312422+AnasSabbag@users.noreply.github.com>
2025-02-23 12:11:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //your JS code here. If required.
 
 function handleSubmit() {
+	
+	const form = document.getElementById('form');
 	let fName=form.querySelector('[name="First Name"]').value;
 	let lName=form.querySelector('[name="Last Name"]').value;
 	let phone=form.querySelector('[name="Phone Number"]').value;
